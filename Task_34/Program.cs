4ed1fb7c01c7ca87dbcb806ef970863881df9957
Danila <Bbunok991@yandex.ru>
2023-07-02 20:46:16 +0300

    Completed the task 34

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -1,34 +1,28 @@
 ﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-[345, 897, 568, 234] -> 2
+[345, 897, 568, 234] -> 2*/
 
-Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-[3, 7, 23, 12] -> 19
-[-4, -6, 89, 6] -> 0
-
- Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
-*/
-
-int InPut(string text)
+int InPut(string text) //Функция для ввода числа.
 {
     System.Console.Write(text);
     return Convert.ToInt32(Console.ReadLine());
 }
-int size = InPut("Enter the lenght of the array: ");
-int left = InPut("Enter the left side: ");
-int right = InPut("Enter the right side: ");
-
-int[] Array(int size, int left, int right)
+int size = InPut("Enter the lenght of the array: "); // Длина массива.
+int left = InPut("Enter the left side: "); // Минимальное число массива.
+int right = InPut("Enter the right side: "); // Максимальное число массива.
+int quantity = 0; // Количество четных чисел в массиве.
+int[] Array(int size, int left, int right) // Функция для заполнения массива и определения количества четных чисел в массиве.
 {
-    Random rand = new Random();
-    int[] array = new int[size];
+    Random rand = new Random(); // Объект класса Random. 
+    int[] array = new int[size]; // Объявление массива.
 
-    for(int i = 0; i < array.Length; i++)
+    for(int i = 0; i < array.Length; i++) // Цикл для заполнения массива
     {
-        array[i] = rand.Next(left, right);
+        array[i] = rand.Next(left, right); // Инициализация элемента массива случайным числом.
+        if(array[i] % 2 == 0) quantity++; // Проверка на четность, и заполнение переменной quantity.
     }
-    return array;
+    return array; // Возвращение массива.
 
 }
-int[] array = Array(size, left, right+1);
-System.Console.Write("[" + string.Join(", ", array) + "]");
+
+int[] array = Array(size, left, right+1); // Получение результата функции в переменную array.
+System.Console.Write("[" + string.Join(", ", array) + $"] -> {quantity}"); // Вывод массива и количества четных элементов в нем.
